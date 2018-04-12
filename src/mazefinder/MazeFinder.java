42c8e38770cee03ca01919b8adbac130a1f98fa5
visionaryJack <10kwatts@gmail.com>
2018-04-11 18:41:50 -0600

    Mazefinder v1

--- src/mazefinder/MazeFinder.java
+++ src/mazefinder/MazeFinder.java
@@ -6,19 +6,34 @@
 package mazefinder;
 
 /**
- *
- * @author 10kwa
+ * Program to take a maze array of chars, with starting position on edge, find start
+ * then recursively find finish marked with F
+ * @author John Watts
  */
 public class MazeFinder {
-    
+    private static final char[][] arIn = new char[][]{//maze array in
+            {'#','#','#','#','#','#','#','#','#','#','#','#'},
+            {'#','.','.','.','#','.','.','.','.','.','.','#'},
+            {'.','.','#','.','#','.','#','#','#','#','.','#'},
+            {'#','#','#','.','#','.','.','.','.','#','.','#'},
+            {'#','.','.','.','.','#','#','#','.','#','.','#'},
+            {'#','#','#','#','.','#','F','#','.','#','.','#'},
+            {'#','.','.','#','.','#','.','#','.','#','.','#'},
+            {'#','#','.','#','.','#','.','#','.','#','.','#'},
+            {'#','.','.','.','.','.','.','.','.','#','.','#'},
+            {'#','#','#','#','.','#','.','#','#','#','.','#'},
+            {'#','.','.','.','.','#','.','#','.','.','.','#'},
+            {'#','#','#','#','#','#','#','#','#','#','#','#'},
+            };
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       Driver runMaze = new Driver();
-       runMaze.findPath();
+       Driver runMaze = new Driver(arIn);
+       runMaze.findStart();//finds and sets start values and direction
+       runMaze.findPath();//recursivly prints while finding finish
+       
     }
-    
 }
